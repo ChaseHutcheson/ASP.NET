933e5d4ede450100634491299c605bd325063dda
Chase Hutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2024-04-30 00:12:52 -0400

    Added more endpoints

---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool isComplete { get; set; }
+    }
+}
